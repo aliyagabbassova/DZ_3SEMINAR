d2de39d6d8e520305245d27bbbc1919001bc92f8
aliya <aliya.gabbassova1911@gmail.com>
2022-12-02 16:43:53 +0600

    Не закончена задача

---
+++ task19/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число. Проверим,является ли оно палиндромом");
+int N = Convert.ToInt32(Console.ReadLine());
+int[]array = new int[5];
+int index = 0,
+for (int i = 0, i < array.length, i++){0,0,0,0,0}
+    if ((array[0] == array [4]) && (array[1] == array[3]))
+{
+    Console.WriteLine("Да"); 
+}      
+   
+
+Console.WriteLine("Нет"); 
+
+
+
+//if ((array[0] == array [4]) && (array[1] == array[3]))
