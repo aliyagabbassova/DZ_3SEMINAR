8185bc8fe749bf7d196538be8cf5f49513e50d9c
aliya <aliya.gabbassova1911@gmail.com>
2022-12-02 15:04:42 +0600

    Задача 23

---
+++ task23/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void Num(int[] num){
+  int count = 0;
+  int length = num.Length;
+  while (count <  length){
+    num[count] = Convert.ToInt32(Math.Pow(count, 3));
+    count++;
+  }
+}
+
+void PrintArray(int[] coll){
+  int count = coll.Length;
+  int index = 0;
+  while(index < count){
+    Console.Write(coll[index]+ " ");
+    index++;
+  }
+} 
+
+int[] array = new int[num+1];
+Num(array);
+PrintArray(array);
